20da0bed1d5b7b869c3a7d8cb99fc4b677b7d24c
Ievgen Voloshchuk <evoloshchuk@gmail.com>
2012-10-10 15:46:26 +0200

    debuging implementation of cache requests

--- flask_cache/__init__.py
+++ flask_cache/__init__.py
@@ -16,6 +16,8 @@ import uuid
 import hashlib
 import inspect
 import exceptions
+import time
+import sys
 
 from types import NoneType
 from functools import wraps
@@ -23,6 +25,7 @@ from functools import wraps
 from werkzeug import import_string
 from werkzeug.contrib.cache import BaseCache, NullCache
 from flask import request, current_app
+from flask import g
 
 JINJA_CACHE_ATTR_NAME = '_template_fragment_cache'
 
@@ -35,6 +38,85 @@ def function_namespace(f):
         return '%s.%s.%s' % (f.__module__, f.im_class.__name__, f.__name__)
     else:
         return '%s.%s' % (f.__module__, f.__name__)
+
+class _CacheRequestInfo():
+    """Cache request debug information"""
+
+
+    def __init__(self, method, args, kwargs, result,
+                 start_time, end_time, context):
+        self.method = method
+        self.args = args
+        self.kwargs = kwargs
+        self.result = result
+        self.start_time = start_time
+        self.end_time = end_time
+        self.context = context
+
+
+    @property
+    def duration(self):
+        return self.end_time - self.start_time
+
+
+    @property
+    def hit(self):
+        return bool(self.result)
+
+
+    @property
+    def parameters(self):
+        parts = []
+        args_repr = ', '.join(["'{}'".format(arg) for arg in self.args])
+        if args_repr:
+            parts.append(args_repr)
+        kwargs_repr = ', '.join(["{}='{}'".format(name, value)
+                                 for name, value in self.kwargs.iteritems()])
+        if kwargs_repr:
+            parts.append(kwargs_repr)
+        return ', '.join(parts)
+
+
+    def __repr__(self):
+        return ('<cache request method="%s" parameters="%s"'
+                ' result="%s" duration=%.03f>') % (self.method, self.parameters,
+                                                   self.result, self.duration)
+
+
+def _record_requests(app):
+    """Check if cache request recording is enabled"""
+    rq = app.config.get('CACHE_RECORD_REQUESTS')
+    if rq is not None:
+        return rq
+    return app.debug
+
+
+def _calling_context(app_path):
+    """Return calling context"""
+    frm = sys._getframe(1)
+    while frm.f_back is not None:
+        name = frm.f_globals.get('__name__')
+        if name and (name == app_path or name.startswith(app_path + '.')):
+            funcname = frm.f_code.co_name
+            return '%s:%s (%s)' % (
+                frm.f_code.co_filename,
+                frm.f_lineno,
+                funcname
+                )
+        frm = frm.f_back
+    return '<unknown>'
+
+
+def get_debug_requests():
+    """In debug mode Flask-Cache will log all the cache requests sent to the
+    server. This information is available until the end of request. If you don't
+    want to enable the DEBUG mode you can also enable the query recording by
+    setting the CACHE_RECORD_REQUESTS config variable to `True`."""
+    if hasattr(g, '_cache_debug_requests'):
+        return g._cache_debug_requests
+    else:
+        return []
+
 
 #: Cache Object
 ################
@@ -108,8 +190,25 @@ class Cache(object):
                             "werkzeug.contrib.cache.BaseCache")
 
     def __getattr__(self, name):
-	"""Proxy to internal cache object"""
-        return getattr(self.cache, name)
+        """Proxy to internal cache object"""
+        if hasattr(self.cache, name):
+            func = getattr(self.cache, name)
+            def wrapper(*args, **kwargs):
+                if not _record_requests(self.app):
+                    return func(*args, **kwargs)
+
+                start_time = time.time()
+
+                result = func(*args, **kwargs)
+                end_time  = time.time()
+                context = _calling_context(self.app.import_name)
+                if not hasattr(g, '_cache_debug_requests'):
+                    g._cache_debug_requests = []
+                g._cache_debug_requests.append(
+                    _CacheRequestInfo(func.__name__, args, kwargs, result,
+                        start_time, end_time, context))
+                return result
+            return wrapper
 
     def cached(self, timeout=None, key_prefix='view/%s', unless=None):
         """
